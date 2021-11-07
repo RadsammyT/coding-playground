a0e3a54858ddee0aadb820828befd4cbaa104147
RadsammyT <32146976+RadsammyT@users.noreply.github.com>
2021-11-06 23:28:10 -0400

    updated grid.java
    added a new method which gets every cell in a string, without spaces.
    yes the spacing is fucking scuffed i fucking know that

--- eclipse-workspace/playground/ttt/Grid.java
+++ eclipse-workspace/playground/ttt/Grid.java
@@ -14,7 +14,12 @@ public class Grid extends Game{
 		grid[i] = as;
 	}
 	
-	
+	public String getAllCells()
+	{
+		return grid[0] +   grid[1] +   grid[2]
+				+grid[3] +   grid[4] +   grid[5] 
+				+grid[6] +   grid[7] +   grid[8]  ;
+	}
 	
 	public String toString()
 	{
