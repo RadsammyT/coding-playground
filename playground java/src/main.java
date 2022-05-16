62b52500fdded3c8ca1c44e9c8d114785750728b
RadsammyT <radsammyt@gmail.com>
2022-05-16 18:29:14 -0400

    capitalized the 'm' in main.java

--- playground java/src/main.java
+++ playground java/src/main.java
@@ -2,7 +2,7 @@ import java.math.*;
 import rad.Timer;
 import javax.swing.*;
 import jFrame.ttt_gui;
-public class main {
+public class Main {
 
     public static void main(String args[]) throws InterruptedException {
         
