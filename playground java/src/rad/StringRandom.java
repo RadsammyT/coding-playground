091c0a2dda1c35a1687368e0bc6a0ecc8979a2f2
RadsammyT <radsammyt@gmail.com>
2022-05-18 17:26:13 -0400

    StringRandom allows for a Random object in params

--- playground java/src/rad/StringRandom.java
+++ playground java/src/rad/StringRandom.java
@@ -4,15 +4,19 @@ import java.util.Random;
 
 public class StringRandom {
     public static String run(String in) {
-        var r = new Random();
+        Random r = new Random();
+        r.setSeed(System.currentTimeMillis());
+        return run(in, r);
+    }
+    
+    public static String run(String in, Random r) {
+        
 		int begin = 0;
-		r.setSeed(System.currentTimeMillis());
+		
 		begin = r.nextInt(in.length());
         return in.substring(
                 begin,
                 r.nextInt(begin, in.length() + 1)
             );
-			
-		
     }
 }
