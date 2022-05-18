39c8a6bd36db30ff0b4495646ff555243d3aee8a
RadsammyT <radsammyt@gmail.com>
2022-05-17 21:31:08 -0400

    compiled c++ source now outputs to an executable folder

--- playground c++/main.cpp
+++ playground c++/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 int main(int, char**) {
-	std::vector<int> v;
-	v.push_back(5);
-    
+	printf("test");
+	vector<int> v;
+
 	return 0;
     
 }
