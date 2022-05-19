734f17666c8770b717b660a7b1f142e33f862d12
RadsammyT <radsammyt@gmail.com>
2022-05-18 21:27:03 -0400

    WHY

--- playground java/src/jFrame/test.java
+++ playground java/src/jFrame/test.java
@@ -1,9 +1,28 @@
 package jFrame;
+import javax.swing.*;
 
-//import javax.swing.*;
-//import javafx;
 public class test {
     
-    
+    public static int init() {
+        // frame
+        JFrame f = new JFrame();
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setSize(500, 500);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // test button, located 100,100. size 50 x 100.
+        // prints "test" on click
+        JButton b = new JButton();
+        b.setText("pee");
+        f.add(b);
+        b.setVisible(true);
+        b.setBounds(100, 100, 50, 100);
+        b.addActionListener(e -> {
+            System.out.println("test");
+        });
+
+        
+        return 0;
+    }
 
 }
