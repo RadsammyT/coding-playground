cc41c6b44e39d8d843273aa2d6301cd6915c63bc
RadsammyT <radsammyt@gmail.com>
2022-06-07 14:45:42 -0400

    fuck

--- playground python/rad/collatz.py
+++ playground python/rad/collatz.py
@@ -1,17 +1,5 @@
-from multipledispatch import dispatch
-
-'''
-inNumber - the number for the function to run the conjecture on
-returnMode - the mode for what thing to return:
-                    0 - number of iterations
-                    1 - odd iterations
-                    2 - even iterations
-                    3 - return all three as a list : [TOTAL_ITERATIONS, ODD_ITERATIONS, EVEN_ITERATIONS]
-
-
-'''
 #i just learned what default arguments are
-def collatzRun(inNumber, returnMode = 0, printSteps = False):
+def collatzRun(inNumber, printSteps = False):
     collatzIterations = 0
     oddIters = 0
     evenIters = 0
@@ -34,16 +22,4 @@ def collatzRun(inNumber, returnMode = 0, printSteps = False):
             
         if inNumber == 1:
             break
-        
-    match(returnMode):   #checks for returnMode input
-        case 0: 
-            return collatzIterations
-        case 1:
-            return oddIters
-        case 2:
-            return evenIters
-        case 3:
-            return [collatzIterations, oddIters, evenIters]
-        case _:
-            return collatzIterations
-            
+        return [collatzIterations, oddIters, evenIters]
