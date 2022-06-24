bfcaf7a28f4fc015d7b5846e0d9984fef3e421fb
RadsammyT <radsammyt@gmail.com>
2022-06-24 12:27:05 -0400

    more leetcode

--- playground java/src/Main.java
+++ playground java/src/Main.java
@@ -7,6 +7,8 @@ import jFrame.ttt_gui;
 import leetcode.leetcode;
 public class Main {
 	public static void main(String args[]) throws InterruptedException, NegativeNumberException, Select {
-		Select.run();
+		// Select.run();
+		System.out.println(Arrays.toString(
+			leetcode.runningSum(new int[] {1,1,1,1,1})));
 	}
 }
--- playground java/src/leetcode/leetcode.java
+++ playground java/src/leetcode/leetcode.java
@@ -6,7 +6,53 @@ import java.util.*;
 public class leetcode {
     // https://leetcode.com/problems/defanging-an-ip-address/
     public static String defangIPAddr(String address) {
-        // System.out.println(address.replaceAll("\\.", "[.]"));
-        return address.replaceAll("\\.", "[.]");
+        System.out.println(address.replaceAll("[.]", "[.]"));
+        return address.replaceAll("[.]", "[.]");
+    }
+    //https://leetcode.com/problems/two-sum/
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {-1, -1};
+    }
+    //https://leetcode.com/problems/build-array-from-permutation/
+    public static int[] BuildArrayFromPermutation(int[] nums) {
+        int[] out = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            out[i] = nums[nums[i]];
+        }
+        // System.out.println(Arrays.toString(nums));
+        // System.out.println(Arrays.toString(out) + "\n");
+        return out;
+    }
+    //https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+    public static int finalValueAfterOperations(String[] ops) {
+        int x = 0;
+        for (String i : ops) {
+            if (i.contains("--")) {
+                x--;
+            }
+            if (i.contains("++")) {
+                x++;
+            }
+        }
+
+        return x;
+    }
+    //https://leetcode.com/problems/running-sum-of-1d-array/
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        int[] out = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            out[i] = sum;
+        }
+
+        return out;
     }
 }
