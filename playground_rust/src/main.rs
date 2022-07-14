da4b097219e039c2e072a879d7a56eab46fe8ed5
RadsammyT <radsammyt@gmail.com>
2022-07-14 18:51:53 -0400

    e

--- playground_rust/src/main.rs
+++ playground_rust/src/main.rs
@@ -13,7 +13,11 @@ mod rad;
 
 
 fn main() {
-    select();
+
+    // select();
+     static SEX_NUMBER: i32 = 69;
+     println!("{}", SEX_NUMBER);
+
 }
 
 fn select() {
@@ -32,6 +36,8 @@ fn select() {
      * you get the point... right?
      */
     let sel: i32 = try_read!().unwrap_or(-1);
+    let mut mainTimer = Timer::new();
+    Timer::start_timer(&mut mainTimer);
     match sel {
         
         1 => {
@@ -42,23 +48,20 @@ fn select() {
         }
         /*
             func({println!("input"); read!()})
+            what the fuck
         */
         3 => { // cursed
             rad::collatz::run({println!("input"); try_read!().unwrap()},
             {println!("print steps? (true/false)"); try_read!().unwrap()});
         }
         4 => {
-            let mut timer = Timer::new();
-            Timer::start_timer(&mut timer);
             let mut vec: Vec<i128> = vec![];
             vec.push(0);
             vec.push(1);
             for i in 0..100 {
                 vec.push(vec[i] + vec[i+1]);
             }
-            Timer::end_timer(&mut timer);
             println!("{:?}", vec);
-            println!("{} seconds", Timer::get_elapsed(&mut timer).unwrap());
         }
         
         _ => {
@@ -66,5 +69,7 @@ fn select() {
             select();
         }
     }
+    Timer::end_timer(&mut mainTimer);
+    println!("{} seconds", Timer::get_elapsed(&mut mainTimer).unwrap());
 }
 
